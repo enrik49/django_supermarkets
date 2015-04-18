1839da77be6f7f9613d1e427b96e18595a4c3d4f
gaudir <gaudirv@gmail.com>
2015-04-18 18:31:12 +0200

    Modificat models.py

--- supermarket/iSupermarket/admin.py
+++ supermarket/iSupermarket/admin.py
@@ -1,3 +1,8 @@
+import models
 from django.contrib import admin
 
-# Register your models here.
+'''admin.site.register(models.Companyia)
+admin.site.register(models.Sucursal)
+admin.site.register(models.Client)
+admin.site.register(models.Marca)
+admin.site.register(models.Producte)'''
--- supermarket/iSupermarket/models.py
+++ supermarket/iSupermarket/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+#from django.core.urlresolvers import reverse
+#from datetime import date
+
+'''class Companyia(models.Model):
+	name = models.TextField()
+
+class Sucursal(models.Model):
+	name = models.TextField()
+
+class Client(models.Model):
+	name = models.TextField()
+
+class Marca(models.Model):
+	name = models.TextField()
+
+class Producte(models.Model):
+	name = models.TextField()'''
