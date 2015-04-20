0e91141a0ea1221d56005470c53f93d130cc409d
Enric Doblas <enrikk49@gmail.com>
2015-04-20 12:33:38 +0200

    Ja podem mostrar el primer html

--- supermarket/iSupermarket/views.py
+++ supermarket/iSupermarket/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.core import urlresolvers
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
+
+from models import Client, Companyia, Marca, Producte, Sucursal
+#from forms import RestaurantForm, DishForm
+
+class Inici(ListView):
+    model = Client
+    template_name = 'index.html'
+    queryset = Client.objects.all()
+
+'''class RestaurantCreate(CreateView):
+    model = Restaurant
+    template_name = 'myrestaurants/form.html'
+    form_class = RestaurantForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(RestaurantCreate, self).form_valid(form)
+
+class DishCreate(CreateView):
+    model = Dish
+    template_name = 'myrestaurants/form.html'
+    form_class = DishForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.restaurant = Restaurant.objects.get(id=self.kwargs['pk'])
+        return super(DishCreate, self).form_valid(form)
+
+def review(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    new_review = RestaurantReview(
+        rating=request.POST['rating'],
+        comment=request.POST['comment'],
+        user=request.user,
+        restaurant=restaurant)
+    new_review.save()
+    return HttpResponseRedirect(urlresolvers.reverse('myrestaurants:restaurant_detail', args=(restaurant.id,)))'''
--- supermarket/supermarket/urls.py
+++ supermarket/supermarket/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'supermarket.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^blog/', include('blog.urls')),
+	url(r'^app/', include('iSupermarket.urls', namespace='iSupermarket')),
+	url(r'^admin/', include(admin.site.urls)),
 )
